544760dc1ef81d81d0c8b1112c5648657a2e8e9a
penglinpeng <1736966390@qq.com>
2018-06-19 15:25:19 +0800

    first modify

---
+++ exercise/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/6/19 0019 15:20
+# @Author  : Lingpeng Peng
+# @FileName: __init__.py.py
+# @Description: 
+# @GitHub  ：https://github.com/ZJU-PLP
---
+++ exercise/exercise01.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/6/19 0019 15:20
+# @Author  : Lingpeng Peng
+# @FileName: exercise01.py
+# @Description: prepare to go
+# @GitHub  ：https://github.com/ZJU-PLP
+
+
