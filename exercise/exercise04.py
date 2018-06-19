c2bea9ca138fa30d7af51ef7f74378ac6eb31236
penglinpeng <1736966390@qq.com>
2018-06-19 17:23:34 +0800

    second modify

---
+++ exercise/exercise04.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/6/19 0019 17:03
+# @Author  : Lingpeng Peng
+# @FileName: exercise04.py
+# @Description: variable and name
+# @GitHub  ：https://github.com/ZJU-PLP
+
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+car_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+
+print("There are ", cars , "cars available.")
+print("There are only", drivers, "drivers available.")
+print("There will be ", car_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people_today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put about", average_passengers_per_car, "in each car.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
