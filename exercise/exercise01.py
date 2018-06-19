38a629b61b2d5d86bf3b690b1dfc33046cc3def4
penglinpeng <1736966390@qq.com>
2018-06-19 15:39:25 +0800

    first modify

--- exercise/exercise01.py
+++ exercise/exercise01.py
@@ -6,4 +6,9 @@
 # @GitHub  ：https://github.com/ZJU-PLP
 
 print("Hello World")
-
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd rather than you 'not'.")
+print("I 'said' do not touch this.")
