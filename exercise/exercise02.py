40f73a953ef2e47c44529b42e508242d0136ff13
penglinpeng <1736966390@qq.com>
2018-06-19 16:32:50 +0800

    second modify

---
+++ exercise/exercise02.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/6/19 0019 16:22
+# @Author  : Lingpeng Peng
+# @FileName: exercise02.py
+# @Description: annotation & #
+# @GitHub  ：https://github.com/ZJU-PLP
+
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print("I could have code like this.")  # and the comment after is ignored
+# you can also use a comment to "disable" or comment out a piece of code:
+
+# print("This won't run.")
+
+
+print("This will run.")
